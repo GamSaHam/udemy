ffce0591c2e2ec631526da907ac35412c206705f
hamgamsa <hamgamsa@gmail.com>
2019-10-14 16:09:33 +0900

    1

--- react course/http--01-starting-setup/src/containers/Blog/Blog.js
+++ react course/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import { Route, Link } from 'react-router-dom';
+
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 // Component Lifecycle -creation
 // constructor(props)
-// componentWillMount();
+// componentWillRouteMount();
 // render()
 // Render Child Components
 // componentDidMount();
@@ -18,16 +21,28 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                <Link
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true'
+                  }}
+                >
+                  New Post
+                </Link>
               </li>
             </ul>
           </nav>
         </header>
 
-        <Posts />
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
