4e332899ef323fe8eb16aa2b512b4ea8d2c606db
hamgamsa <hamgamsa@gmail.com>
2019-10-14 15:11:08 +0900

    1

--- react course/http--01-starting-setup/src/containers/Blog/Blog.js
+++ react course/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from './FullPost/FullPost';
-import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 // Component Lifecycle -creation
 // constructor(props)
@@ -15,54 +11,7 @@ import './Blog.css';
 // componentDidMount();
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  };
-
-  componentDidMount() {
-    axios
-      .get('/posts')
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => {
-          return {
-            ...post,
-            author: 'Max'
-          };
-        });
-
-        this.setState({ posts: updatedPosts });
-      })
-      .catch(error => {
-        // console.log(error);
-        this.setState({ error: true });
-      });
-  }
-
-  postSelectedHandler = id => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
-    let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
-
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => {
-              this.postSelectedHandler(post.id);
-            }}
-          />
-        );
-      });
-    }
-
     return (
       <div className="Blog">
         <header>
@@ -78,7 +27,7 @@ class Blog extends Component {
           </nav>
         </header>
 
-        <section className="Posts">{posts}</section>
+        <Posts />
       </div>
     );
   }
