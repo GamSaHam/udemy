9e0901cd5df1984068831538c95e71cd2bd8652a
hamgamsa <hamgamsa@gmail.com>
2019-10-29 13:22:11 +0900

    1

--- react course/routing--assignment-problem/src/containers/Course/Course.js
+++ react course/routing--assignment-problem/src/containers/Course/Course.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
-    }
+  componentDidMount() {
+    console.log(this.props.match.params.id);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>_COURSE_TITLE_</h1>
+        <p>You selected the Course with ID: _ID_</p>
+      </div>
+    );
+  }
 }
 
 export default Course;
