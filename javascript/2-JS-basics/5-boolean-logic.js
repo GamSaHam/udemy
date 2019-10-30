751df6e1e5ce5fd8bdc3f4adfc6b44a9e51eef8f
hamgamsa <hamgamsa@gmail.com>
2019-10-30 16:40:40 +0900

    1

--- javascript/2-JS-basics/5-boolean-logic.js
+++ javascript/2-JS-basics/5-boolean-logic.js
@@ -2,30 +2,20 @@
     Boolean logic
 */
 var firstName = "John";
-var age = 16;
+var age = 20;
 
 if(age < 13){
     console.log(firstName + " is a boy.");
     
-}else if(){ // Between 13 and 20 === age >= 13 AND age < 20
+}else if(age >= 13 && age < 20){ // Between 13 and 20 === age >= 13 AND age < 20
     console.log(firstName + " is a teenager.");
-    
-    
+        
+}else if(age >= 20 && age < 30){
+    console.log(firstName + " is a young man.")     
+         
 }else{
     console.log(firstName + " is a man.");
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
