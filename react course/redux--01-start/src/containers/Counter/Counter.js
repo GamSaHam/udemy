3f60fce44326aea07f8f34bd843510b42e16238d
hamgamsa <hamgamsa@gmail.com>
2019-11-04 16:52:06 +0900

    1

--- react course/redux--01-start/src/containers/Counter/Counter.js
+++ react course/redux--01-start/src/containers/Counter/Counter.js
@@ -19,9 +19,22 @@ class Counter extends Component {
         />
         <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
         <CounterControl
-          label="Subtract 8"
+          label="Subtract 15"
           clicked={this.props.onSubtractCounter}
         />
+
+        <hr />
+        <button onClick={this.props.onStoreResult}>Stroe Result</button>
+        <ul>
+          {this.props.storedResults.map(strResult => (
+            <li
+              key={strResult.id}
+              onClick={() => this.props.onDeleteResult(strResult.id)}
+            >
+              {strResult.value}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -29,7 +42,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
@@ -37,8 +51,10 @@ const mapDispatchToProps = dispatch => {
   return {
     onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
     onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-    onAddCounter: () => dispatch({ type: 'ADD' }),
-    onSubtractCounter: () => dispatch({ type: 'SUBTRACT' })
+    onAddCounter: () => dispatch({ type: 'ADD', val: 10 }),
+    onSubtractCounter: () => dispatch({ type: 'SUBTRACT', val: 15 }),
+    onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+    onDeleteResult: id => dispatch({ type: 'DELETE_RESULT', resultElId: id })
   };
 };
 
--- react course/redux--01-start/src/store/reducer.js
+++ react course/redux--01-start/src/store/reducer.js
@@ -1,30 +1,54 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      counter: state.counter + 1
-    };
-  }
+  switch (action.type) {
+    case 'INCREMENT':
+      const newState = Object.assign({}, state);
+      newState.counter = state.counter + 1;
+      return newState;
 
-  if (action.type === 'DECREMENT') {
-    return {
-      counter: state.counter - 1
-    };
-  }
+    case 'DECREMENT':
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
 
-  if (action.type === 'ADD') {
-    return {
-      counter: state.counter + 10
-    };
-  }
+    case 'ADD':
+      return {
+        ...state,
+        counter: state.counter + action.val
+      };
 
-  if (action.type === 'SUBTRACT') {
-    return {
-      counter: state.counter - 8
-    };
+    case 'SUBTRACT':
+      return {
+        ...state,
+        counter: state.counter - action.val
+      };
+    //new Date().getUTCMilliseconds();
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({
+          id: new Date().getUTCMilliseconds(),
+          value: state.counter
+        })
+      };
+
+    case 'DELETE_RESULT':
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1);
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultElId
+      );
+
+      return {
+        ...state,
+        results: updatedArray
+      };
   }
 
   return state;
