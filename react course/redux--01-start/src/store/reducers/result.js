90f2e8df3450c6fed3945fa00ecf341a1f65715d
hamgamsa <hamgamsa@gmail.com>
2019-11-05 13:38:50 +0900

    1

--- react course/redux--01-start/src/store/reducers/result.js
+++ react course/redux--01-start/src/store/reducers/result.js
@@ -1,43 +1,22 @@
+import * as actionTypes from '../actions';
+
 const initialState = {
-  counter: 0,
   results: []
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      const newState = Object.assign({}, state);
-      newState.counter = state.counter + 1;
-      return newState;
-
-    case 'DECREMENT':
-      return {
-        ...state,
-        counter: state.counter - 1
-      };
-
-    case 'ADD':
-      return {
-        ...state,
-        counter: state.counter + action.val
-      };
-
-    case 'SUBTRACT':
-      return {
-        ...state,
-        counter: state.counter - action.val
-      };
     //new Date().getUTCMilliseconds();
-    case 'STORE_RESULT':
+    case actionTypes.STORE_RESULT:
       return {
         ...state,
         results: state.results.concat({
           id: new Date().getUTCMilliseconds(),
-          value: state.counter
+          value: action.result
         })
       };
 
-    case 'DELETE_RESULT':
+    case actionTypes.DELETE_RESULT:
       // const id = 2;
       // const newArray = [...state.results];
       // newArray.splice(id, 1);
