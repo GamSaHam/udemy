58ff0817db5eabd3a664a0c5b0fbb71262c22a08
hamgamsa <hamgamsa@gmail.com>
2019-11-05 15:20:27 +0900

    1

---
+++ react course/redux--assignment-2-problem/src/store/reducer.js
@@ -0,0 +1,33 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+  persons: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_PERSON:
+      const newPerson = {
+        id: Math.random(), // not really unique but good enough here!
+        name: action.personData.name,
+        age: action.personData.age
+      };
+
+      return {
+        ...state,
+        persons: state.persons.concat(newPerson)
+      };
+
+    case actionTypes.REMOVE_PERSON:
+      return {
+        ...state,
+        // this.setState( ( prevState ) => {
+        //    return { persons: prevState.persons.filter(person => person.id !== personId)}
+        //} );
+        persons: state.persons.filter(person => person.id !== action.personId)
+      };
+  }
+  return state;
+};
+
+export default reducer;
