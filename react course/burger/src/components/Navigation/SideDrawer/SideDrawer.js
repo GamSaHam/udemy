bbfb81cfac91199437bf3efbdbee1356de791b0f
hamgamsa <hamgamsa@gmail.com>
2019-11-20 14:23:59 +0900

    1

--- react course/burger/src/components/Navigation/SideDrawer/SideDrawer.js
+++ react course/burger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.css';
-import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxiliary/Auxiliary";
 
 const sideDrawer = props => {
   let attachedClasses = [classes.SideDrawer, classes.Close];
@@ -16,12 +16,12 @@ const sideDrawer = props => {
   return (
     <Aux>
       <Backdrop show={props.open} clicked={props.closed} />
-      <div className={attachedClasses.join(' ')}>
+      <div className={attachedClasses.join(" ")}>
         <div className={classes.Logo}>
           <Logo />
         </div>
         <nav>
-          <NavigationItems />
+          <NavigationItems isAuthenticated={props.isAuth} />
         </nav>
       </div>
     </Aux>
