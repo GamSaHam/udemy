462d9a0083bab1ed2df326e803c6cd6988e69bdd
hamgamsa <hamgamsa@gmail.com>
2019-12-02 16:17:33 +0900

    1

--- react course/burger/src/hoc/withErrorHandler/withErrorHandler.js
+++ react course/burger/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,55 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Modal from "../../components/UI/Modal/Modal";
 import Aux from "../Auxiliary/Auxiliary";
+import useHttpErrorHandler from "../../hooks/http-error-handler";
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return props => {
-    const [error, setError] = useState(null);
-
-    // componentWillMount() {
-    //   this.reqInterceptor = axios.interceptors.request.use(req => {
-    //     this.setState({ error: null });
-    //     return req;
-    //   });
-    //   this.resInterceptor = axios.interceptors.response.use(
-    //     res => res,
-    //     error => {
-    //       this.setState({ error: error });
-    //     }
-    //   );
-    // }
-
-    const reqInterceptor = axios.interceptors.request.use(req => {
-      setError(null);
-      return req;
-    });
-    const resInterceptor = axios.interceptors.response.use(
-      res => res,
-      err => {
-        setError(err);
-      }
-    );
-
-    // componentWillUnmount() {
-    //   axios.interceptors.request.eject(this.reqInterceptor);
-    //   axios.interceptors.response.eject(this.resInterceptor);
-    // }
-
-    useEffect(() => {
-      return () => {
-        axios.interceptors.request.eject(reqInterceptor);
-        axios.interceptors.response.eject(resInterceptor);
-      };
-    }, [reqInterceptor, resInterceptor]);
-
-    const errorConfirmedHandler = () => {
-      setError(null);
-    };
+    const [error, clearError] = useHttpErrorHandler(axios);
 
     return (
       <Aux>
-        <Modal show={error} modalClosed={errorConfirmedHandler}>
+        <Modal show={error} modalClosed={clearError}>
           {error ? error.message : null}
         </Modal>
         <WrappedComponent {...props} />
