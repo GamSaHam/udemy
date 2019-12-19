3bbca78304638cd186fbfa1aef5953c32644e1de
GamSaHam <cleverones@naver.com>
2019-12-19 15:05:15 +0900

    1

--- javascript/5-advanced-JS/script.js
+++ javascript/5-advanced-JS/script.js
@@ -1,38 +1,40 @@
 // Function constructor
 
 var john = {
-    name: 'John',
-    yearOfBirth: 1990,
-    job:'teacher'
+  name: "John",
+  yearOfBirth: 1990,
+  job: "teacher"
 };
 
-var Person = function(name, yearOfBirth, job){
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-//    this.calculateAge = function(){
-//        console.log(2016 - this.yearOfBirth);
-//    }
-}
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+  //    this.calculateAge = function(){
+  //        console.log(2016 - this.yearOfBirth);
+  //    }
+};
 
 Person.prototype.calculageAge = function() {
-    console.log(2016 - this.yearOfBirth);
-    
-}
+  console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = "Smith";
 
 // new creates empty object
-var john = new Person('John', 1990, 'teacher');
+var john = new Person("John", 1990, "teacher");
 
 john.calculageAge();
 
 // array has a prototype chain
 // prototype chain method likes push unshift
 
-var arr = [1,2,3];
+var arr = [1, 2, 3];
 
-console.log(arr.hasOwnProperty('length')); // true
-console.log(john instanceof(Person)) // true
+john.hasOwnProperty("job"); // true
+john.hasOwnProperty("lastName"); // false
+john instanceof Person; // true
 
+var x = [2, 4, 6];
 
-
-
+//console.info(x); // use chrome developer
