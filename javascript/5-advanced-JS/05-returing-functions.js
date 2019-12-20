0a306d57a9ce5f2e796a52ccb8fa0032b6ce6683
GamSaHam <cleverones@naver.com>
2019-12-20 16:04:00 +0900

    1

---
+++ javascript/5-advanced-JS/05-returing-functions.js
@@ -0,0 +1,23 @@
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return function(name) {
+      console.log(name + ", can you please explanin what UX design is?");
+    };
+  } else if (job === "teacher") {
+    return function(name) {
+      console.log("What subject do you teach, " + name + "?");
+    };
+  } else {
+    return function(name) {
+      console.log("Hello " + name + ", what do you do?");
+    };
+  }
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+teacherQuestion("John");
+
+var designerQuestion = interviewQuestion("designer");
+designerQuestion("John");
+
+interviewQuestion("teacher")("Mark");
