b9fc4219e18ff5c994a723b052786c02decd2625
GamSa Ham <cleverones@naver.com>
2020-01-31 10:52:12 +0900

    1

--- vuejs/14-vue-cli/src/App.vue
+++ vuejs/14-vue-cli/src/App.vue
@@ -1,17 +1,15 @@
 <template>
-    <h1>Hello World!</h1>
+    <app-servers></app-servers>
 </template>
 
 <script>
-    export default {
-        
-    }
-
-
-
 
 </script>
 
 <style>
 
 </style>
+
+
+
+
