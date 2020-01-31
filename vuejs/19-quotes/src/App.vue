717ec308c8365262e9cd0308649b0c7d15f78832
GamSa Ham <cleverones@naver.com>
2020-01-31 16:46:34 +0900

    1

--- vuejs/19-quotes/src/App.vue
+++ vuejs/19-quotes/src/App.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="container">
-    
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from "./components/QuoteGrid.vue";
+    import NewQuote from "./components/NewQuote.vue";
+
+
     export default {
         data: function(){
                 return{
@@ -12,9 +17,17 @@
                         'Just a Quote to see something'
                     ],
                     maxQuotes: 10
-
-
                 }
+        },
+        methods:{
+            newQuote(quote){
+                this.quotes.push(quote);
+
+            }
+        },
+        components:{
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
 
         }
     }
