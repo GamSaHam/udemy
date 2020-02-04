205581470918b3979f71ab51cd343a040fd709fa
GamSaHam <cleverones@naver.com>
2020-02-04 23:00:33 +0900

    1

---
+++ spring/demo/src/main/java/com/example/demo/dto/BaseBallCoach.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+public class BaseBallCoach extends Coach{
+
+    @Override
+    public String getDailyWorkOut(){
+        return "Spend 30 minutes on batting practice";
+    }
+}
---
+++ spring/demo/src/main/java/com/example/demo/dto/Coach.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+
+public class Coach {
+
+    public String getDailyWorkOut(){
+        return "Run a hard 5k";
+    }
+
+
+}
