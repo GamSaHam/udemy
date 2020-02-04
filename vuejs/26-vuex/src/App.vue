23759e720b446095811056ac332b2a150919b4d6
GamSa Ham <cleverones@naver.com>
2020-02-04 13:27:48 +0900

    1

--- vuejs/26-vuex/src/App.vue
+++ vuejs/26-vuex/src/App.vue
@@ -1,26 +1,49 @@
 <template>
     <div class="container">
         
-        <p>{{count}}</p>
-        
-        <p>{{this.$store.getters.doneTodos}}</p>
+        <p>count - {{this.$store.state.count}}</p>
+        <p>todosCount - {{this.$store.getters.doneTodosCount}}</p>
+        <p>doneTodosCount - {{doneTodosCount}}</p>
         <button @click = "increase()">increase</button>
+        <button @click = "increasePayload(10)">increase(payload)</button>
+        <button @click = "increaseDispatch()">increaseDispatch()</button>
+
+        <hr>
+        <p>module a count - {{this.$store.state.a.count}}</p>        
+        <p>module b count - {{this.$store.state.b.count}}</p>        
         
     </div>
 </template>
 
 <script>
+    import {mapGetters} from "vuex";
+
     export default {
         methods:{
             increase(){
-                this.$store.state.count += 1;
+                this.$store.commit('increment');
+            },
+            increasePayload(payload){
+                this.$store.commit('increasePayload',{amount: payload});
+            },
+            increaseDispatch(){
+                this.$store.dispatch('increment');
+
             }
+
         },
         computed:{
-            count(){
+            myMessage : function(){
+                
                 return this.$store.state.count;
             }
-        }
+        },
+        computed: mapGetters({
+            doneTodosCount:'doneTodosCount'
+        })
+        
+
+        
     
     }
 </script>
--- vuejs/26-vuex/src/main.js
+++ vuejs/26-vuex/src/main.js
@@ -5,12 +5,27 @@ import App from './App.vue'
 
 Vue.use(Vuex);
 
+
+const moduleA = {
+  state : {
+    count: 1
+  }
+}
+
+const moduleB = {
+  state : {
+    count : 2
+  }
+}
+
+
 const store = new Vuex.Store({
     state:{
         todos : [
           {id : 1, text: '...', done: true},
           {id : 2, text: '...', done: false}
-        ]
+        ],
+        count : 0
     },
     mutations: {  // store.commit('increment')
         increment(state){
@@ -23,8 +38,33 @@ const store = new Vuex.Store({
       },
       doneTodosCount: (state, getters) =>{
         return getters.doneTodos.length;
+      },
+      // getTodoById: (state) => (id) =>{
+      //   return state.todos.find(todo => too.id === id)
+      // }
+
+    },
+    mutations: {
+      increment(state){
+
+          state.count++;
+      },
+      increasePayload(state, payload){
+          state.count += payload.amount;
       }
+
+    },
+    actions: {
+      increment(context){
+        context.commit('increment')
+      }
+    },
+    modules: {
+      a : moduleA,
+      b : moduleB
     }
+
+
 
 
 })
