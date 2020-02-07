a891ba13884218bf279d0d4c612e328163b73804
GamSa Ham <cleverones@naver.com>
2020-02-07 10:02:09 +0900

    1

--- spring/02-annotations/src/main/java/com/example/demo/dto/TennisCoach.java
+++ spring/02-annotations/src/main/java/com/example/demo/dto/TennisCoach.java
@@ -4,9 +4,14 @@ import com.example.demo.services.FortuneService;
 import com.example.demo.services.HappyFortuneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component(value = "tennisCoach")
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
@@ -25,6 +30,19 @@ public class TennisCoach implements Coach {
 
     public String getDailyFortune(){
             return fortuneService.getFortune();
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println(">> TenisCoach: inside of doMyStartupStuff()");
+
+    }
+
+    // define my destory method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println(">> TenisCoach: inside of doMyCleanupStuff()");
     }
 
 
