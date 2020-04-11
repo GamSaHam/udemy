61511cb9a7a7feda6f1001ed890707dc02a0eadb
GamSaHam <cleverones@naver.com>
2020-04-11 17:25:27 +0900

    1

---
+++ spring/spring-boot/17-spring-security-demo-01-base-app/src/main/java/com/example/demo/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
